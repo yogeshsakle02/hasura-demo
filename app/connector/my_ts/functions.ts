f0a9123fc6e744fdf0af28a90e73072a22a0f5ee
Praveen Joshi <praveen.joshi@gemsessence.com>
2025-07-22 14:37:09 +0530

    changes complex query

--- app/connector/my_ts/functions.ts
+++ app/connector/my_ts/functions.ts
@@ -5,7 +5,29 @@ export function hello(name?: string) {
   return `hello ${name ?? "world"}`;
 }
 
-// This is goes as mutation.
-export function hi(name?: string) {
-  return `hi ${name ?? "world"}`;
+/**
+ * @readonly Exposes the function as an NDC function (the function should only query data without making modifications)
+ */
+  
+export function userInfo(name?: string) {
+  return {
+    name: "yogesh",
+    age: 25,
+    email: "Yogesh@gemsessnece.com",
+    dob: new Date(),
+    isMarried: false,
+    mobileNo: 1234567890,
+      address: [{
+      addressType: 'home'  ,
+      houseNo: 123,
+      homeAddress: "sattelite township",
+      area: "bijalpur",
+      },
+      {
+      addressType: 'office'  ,
+      houseNo: 321,
+      homeAddress: "futikhothi",
+      area: "indore",
+    }]
+  }
 }
